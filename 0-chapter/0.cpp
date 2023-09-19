d05c41b022b2bc2a4f585a68eb4b20bf4e4ad887
Luke Sheppard <sheppiegit@gmail.com>
2023-09-19 18:42:36 -0400

    intro chapter.

---
+++ 0-chapter/0.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This (*) is a quote, and this is a (\\) is a backslash" <<std::endl;
+    std::cout << "Hello,\n World!"<<std::endl;
+    return 0;
+}
---
+++ 0-chapter/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//a small test c++ program
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
