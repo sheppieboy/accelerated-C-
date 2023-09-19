53dbda083ecd99be0e0aaf3b0b3459cd8ccdbfad
Luke Sheppard <sheppiegit@gmail.com>
2023-09-19 19:14:52 -0400

    updated greetings for style.

--- 1-chapter/greetings.cpp
+++ 1-chapter/greetings.cpp
@@ -10,8 +10,24 @@ int main(){
     std::string name; //define a name
     std::cin >> name; //read into name
 
-    //write a greeting
-    std::cout << "Hello, " << name << "!" << std::endl;
+    //build greetings
+    const std::string greetings = "Hello, " + name + "!";
+
+    //build the second and fourth lines of the output
+    const std::string spaces(greetings.size(), ' ');
+    const std::string second = "* "+spaces+" *";
+
+    //build the first and fifth lines
+    const std::string first(second.size(), '*');
+
+    //write it all
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second <<std::endl;
+    std::cout << "* " << greetings << " *" <<std::endl;
+    std::cout << second <<std::endl;
+    std::cout << first<<std::endl;
     return 0;
+    
 
 }
