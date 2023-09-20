db0d0f3af3236e27d93a5cf1f9615e25633bec01
Luke Sheppard <sheppiegit@gmail.com>
2023-09-20 17:40:48 -0400

    added filtering passed on passing or failing below a 60% grade.

---
+++ 5-chapter/4-chapter/functions-grades.cpp
@@ -0,0 +1,72 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include "grade.h"
+#include "Student_info.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::string;
+using std::streamsize;
+using std::vector;
+using std::domain_error;
+using std::max;
+
+//predicate to determine whether a student failed
+bool fgrade(const Student_info& s){
+    return grade(s) < 60;
+}
+
+vector<Student_info> extract_fails(vector<Student_info>& students){
+    vector<Student_info> pass, fail;
+    for(vector<Student_info>::size_type i = 0; i != students.size(), i++){
+        if(fgrade(students[i])){
+            fail.push_back(students[i]);
+        }else{
+            pass.push_back(students[i]);
+        }
+    }
+    students = pass;
+    return fail;
+}
+
+int main(){
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen =0; //length of longest name
+    //read and store all the students data
+    while(read(cin, record)){
+        //find longest length name
+        maxlen = max(maxlen, record.name.size());
+        students.push_back(record);
+    }
+
+    //sort
+    sort(students.begin(), students.end(), compare);
+    
+    //write the anmes and grades
+    for(vector<Student_info>::size_type i = 0; i != students.size(); i++){
+        //write name, padded on teh right to maxlen + 1 character
+        cout << students[i].name << string(maxlen+1 -students[i].name.size(), ' ');
+        //compute and write the grade
+        try{
+            double final_grade = grade(students[i]);
+            streamsize prec = cout.precision();
+            cout << setprecision(3) << final_grade << setprecision(prec);
+        }catch(domain_error e){
+            cout << e.what();
+        }
+        cout << endl;
+    }
+
+    return 0;
+
+}
+
+
