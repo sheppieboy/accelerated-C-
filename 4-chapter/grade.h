b8d07c7a4503f3c08f7afd573741f91648305b9c
Luke Sheppard <sheppiegit@gmail.com>
2023-09-19 23:29:41 -0400

    fixed typo.

--- 4-chapter/grade.h
+++ 4-chapter/grade.h
@@ -5,7 +5,7 @@
 #include "Student_info.h"
 
 double grade(double, double, double);
-double grade(double, double, const std::vector<double>);
+double grade(double, double, const std::vector<double>&);
 double grade(const Student_info&);
 
 #endif
